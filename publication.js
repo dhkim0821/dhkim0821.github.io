0950ba448fe27ef9b5a461582ca0578b4416b9fe
dhkim0821 <alex.d.kim0821@gmail.com>
2019-12-22 17:04:14 -0500

    update publication

--- publication.js
+++ publication.js
@@ -20,6 +20,15 @@ document.write('\
     \
     <br\> \
     <h5>Peer Reviewed Conference Proceedings</h5>\
+    <h4>Bi-Modal Hemispherical Sensor: A Unifying Solution for Three Axis Force and Contact Angle Measurement</h4> \
+    <p> Meng Yee Chuah, Lindsay Epstein, <font color="olive">Donghyun Kim</font>, Juan Romero, and Sangbae Kim, International Conference on Intelligent Robots and Systems (IROS), 2019. <a href="papers/2019_IROS_Sensor.pdf"> [Paper]</a> </p> \
+    \
+    <h4>Control of A High Performance Bipedal Robot using Viscoelastic Liquid Cooled Actuators</h4> \
+    <p>Junhyeok Ahn, <font color="olive">Donghyun Kim</font>, Seunghyeon Bang, Nick Paine, and Luis Sentis, International Conference of Humanoid Robots, 2019. <a href="papers/2019_humanoid.pdf"> [Paper]</a> </p> \
+    \
+    <h4>Control Scheme and Uncertainty Considerations for Dynamic Balancing of Passive-Ankled Bipeds and Full Humanoids</h4> \
+    <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Hochul Hwang, and Luis Sentis, International Conference of Humanoid Robots, 2018. <a href="papers/2018_humanoid_mercury.pdf"> [Paper]</a> </p> \
+    \
     <h4>Computationally-Robust and Efficient Prioritized Whole-Body Controller with Contact Constraints </h4> \
     <p> <font color="olive">Donghyun Kim</font>, Jaemin Lee, Junhyeok Ahn, Orion Campbell, Hochul Hwang, and Luis Sentis, International Conference on Intelligent Robots and Systems (IROS), 2018. <a href="papers/2018_IROS_WBDC.pdf"> [Paper]</a> </p> \
     \
@@ -27,7 +36,7 @@ document.write('\
     <p> Junhyeok Ahn, Orion Campbell, <font color="olive">Donghyun Kim</font>, and Luis Sentis, International Conference on Intelligent Robots and Systems (IROS), 2018. <a href="papers/2018_IROS_Planning.pdf"> [Paper]</a> </p> \
     \
     <h4>Investigations of Viscoelastic Liquid Cooled Actuators Applied for Dynamic Motion Control of Legged Systems </h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Orion Campbell, Junhyeok Ahn, Luis Sentis, Nicholas Paine, International Conference of Humanoid Robots, 2017. <a href="papers/vlca_humanoid_2017.pdf"> [Paper]</a> </p> \
+    <p> <font color="olive">Donghyun Kim</font>, Orion Campbell, Junhyeok Ahn, Luis Sentis, Nicholas Paine, International Conference of Humanoid Robots, 2017. <a href="papers/2017_humanoid_vlca.pdf"> [Paper]</a> </p> \
     \
     <h4>Dynamic Behaviors on the NAO Robot With Closed-Loop Whole Body Operational Space Control</h4> \
     <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Luis Sentis, International Conference of Humanoid Robots, 2016. <a href="papers/humanoid_nao.pdf"> [Paper]</a> <a href="poster/humanoid2016_poster.png"> \[Poster]</a> </p> \
@@ -59,6 +68,9 @@ document.write('\
     \
     <br\> \
     <h5> Preprints </h5>\
+    <h4>Highly Dynamic Quadruped Locomotion via Whole-Body Impulse Control and Model Predictive Control</h4> \
+    <p> <font color="olive">Donghyun Kim</font>, Jared Di Carlo, Benjamin Katz, Gerardo Bledt, and Sangbae Kim, in arXiv, 2019. <a href="papers/arxiv_wbic_mini-cheetah.pdf"> \[Paper]</a> </p> \
+    \
     <h4>Dynamic Locomotion For Passive-Ankle Biped Robots And Humanoids Using Whole-Body Locomotion Control</h4> \
     <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Jaemin Lee, Junhyeok Ahn, Jianwen Luo, and Luis Sentis, in arXiv, 2019. <a href="papers/arxiv_mercury.pdf"> \[Paper]</a> </p> \
     \
