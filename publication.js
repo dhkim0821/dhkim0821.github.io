902c01cbc5e1c529c2074867214da297e40ba582
dhkim0821 <alex.d.kim0821@gmail.com>
2020-03-08 21:54:20 -0400

    publication update

--- publication.js
+++ publication.js
@@ -1,89 +1,93 @@
 document.write('\
-    <div id="publication" class="publication services">\
-    <!-- container -->\
-    <div class="container">\
-    <h3 class="w3layouts-title wow fadeInDown animated" data-wow-delay=".5s">Publication</h3> \
-    <div class="carousel-inner" role="listbox" style=" width:100%; height: 100% !important;"> \
-    <div class="publication-left"> </div> \
-    <div class="publication-right"> \
-    <h5>Journals</h5> \
-    <h4>Investigations of a Robotic Testbed with Viscoelastic Liquid Cooled Actuators </h4> \
-    <p> <font color="olive">Donghyun Kim</font>, J. Ahn, O. Campbell, N. Paine, L. Sentis, in Transaction on Mechatronics, 2018. <a href="papers/TMech_2018.pdf"> \[Paper]</a> </p> \
-    \
-     <h4>Stabilizing Series-Elastic Point-Foot Bipeds Using Whole-Body Operational Space Control</h4> \
-    <p> [Finalist, Best WBC paper 2016] [Finalist, Best WBC-Video 2016] </p> \
-    <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Benito Fernandez, Luis Sentis, in Transaction on Robotics, 2016. <a href="papers/tro.pdf"> \[Paper]</a> </p> \
-    \
-    <h4>Kinematic feedback control laws for generating natural arm movements</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Cheongjae Jang, Frank C. Park, Bioinspiration & Biomimetics, 2013. <a href="papers/kin_feedback_arm_movements.pdf"> \[Paper]</a> </p> \
-    \
-    \
-    <br\> \
-    <h5>Peer Reviewed Conference Proceedings</h5>\
-    <h4>Vision Aided Dynamic Exploration of Unstructured Terrain with a Small-Scale Quadruped Robot</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Daniel Carballo, Jared Di Carlo, Benjamine Katz, Gerardo Bledt, Bryan Lim, and Sangbae Kim, International Conference on Robotics and Automation (ICRA), 2020. <a href="papers/2020_ICRA_Vision.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Bi-Modal Hemispherical Sensor: A Unifying Solution for Three Axis Force and Contact Angle Measurement</h4> \
-    <p> Meng Yee Chuah, Lindsay Epstein, <font color="olive">Donghyun Kim</font>, Juan Romero, and Sangbae Kim, International Conference on Intelligent Robots and Systems (IROS), 2019. <a href="papers/2019_IROS_Sensor.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Control of A High Performance Bipedal Robot using Viscoelastic Liquid Cooled Actuators</h4> \
-    <p>Junhyeok Ahn, <font color="olive">Donghyun Kim</font>, Seunghyeon Bang, Nick Paine, and Luis Sentis, International Conference of Humanoid Robots, 2019. <a href="papers/2019_humanoid.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Control Scheme and Uncertainty Considerations for Dynamic Balancing of Passive-Ankled Bipeds and Full Humanoids</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Hochul Hwang, and Luis Sentis, International Conference of Humanoid Robots, 2018. <a href="papers/2018_humanoid_mercury.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Computationally-Robust and Efficient Prioritized Whole-Body Controller with Contact Constraints </h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Jaemin Lee, Junhyeok Ahn, Orion Campbell, Hochul Hwang, and Luis Sentis, International Conference on Intelligent Robots and Systems (IROS), 2018. <a href="papers/2018_IROS_WBDC.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Fast Kinodynamic Bipedal Locomotion Planning with Moving Obstacles</h4> \
-    <p> Junhyeok Ahn, Orion Campbell, <font color="olive">Donghyun Kim</font>, and Luis Sentis, International Conference on Intelligent Robots and Systems (IROS), 2018. <a href="papers/2018_IROS_Planning.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Investigations of Viscoelastic Liquid Cooled Actuators Applied for Dynamic Motion Control of Legged Systems </h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Orion Campbell, Junhyeok Ahn, Luis Sentis, Nicholas Paine, International Conference of Humanoid Robots, 2017. <a href="papers/2017_humanoid_vlca.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Dynamic Behaviors on the NAO Robot With Closed-Loop Whole Body Operational Space Control</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Luis Sentis, International Conference of Humanoid Robots, 2016. <a href="papers/humanoid_nao.pdf"> [Paper]</a> <a href="poster/humanoid2016_poster.png"> \[Poster]</a> </p> \
-    \
-    <h4>A Method for Dynamically Balancing a Point Foot Robot</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Gray Thomas, Luis Sentis, International Conference of Humanoid Robots, 2015. <a href="papers/humanoid_2015_hume.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Continuous Cyclic Stepping on 3D Point-Foot Biped Robots Via Constant Time to Velocity Reversal</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Gray Thomas, Luis Sentis, 13th International Conference on Control, Automation, Robotics & Vision (ICARCV 2014) <a href="papers/ICARCV_2014_Hume.pdf"> [Paper]</a> </p> \
-    \
-    <h4>Empirical Modifications to a Phase Space Planner Which Compensates for Low Stiffness Actuation in a Planar, Point-foot, Biped Robot</h4> \
-    <p><font color="olive"> Donghyun Kim</font>, Ye Zhao, Gray Thomas, Proceedings of the ASME 2014 Dynamic Systems and Control Conference (DSCC 2014) <a href="papers/DSCC_2014_Hume.pdf"> [Paper]</a>  </p> \
-    \
-    <h4>Phase Space Planning and Robust Control for Data-Driven Locomotion Behaviors</h4> \
-    <p> Ye Zhao, <font color="olive">Donghyun Kim</font>, Benito Fernandez, Luis Sentis, International Conference of Humanoid Robots, 2013. <a href="papers/2013_humanoid.pdf"> [Paper]</a> </p> \
-    \
-    \
-    <br\> \
-    <h5>Abstracts and Posters</h5>\
-    <h4>Whole-Body Opertation Space Control: Undirectional Walking of a Point-Foot Series Elastic Biped</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Luis Sentis, Dynamic Walking, 2015. <a href="papers/DW15Abstract.pdf"> \[Paper]</a> \
-    \
-    <h4>Poster: Hybrid Multi-Contact Dynamics for Wedge Jumping Locomotion Behaviors</h4> \
-    <p> Ye Zhao, <font color="olive">Donghyun Kim</font>, Gray Thomas, Luis Sentis, HSCC, 2015. <a href="papers/HSCC-15-Locomotion-final.pdf"> \[Paper]</a> \
-     \
-    <h4>Towards Agility in Compliant Point-Foot Bipeds</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Luis Sentis, Dynamic Walking, 2014. <a href="papers/DW14Abstract.pdf"> \[Paper]</a> <a href="poster/Toward_Agility_in_Compliant_Point_Foot_Bipeds.pdf"> \[Poster]</a> </p> \
-    \
-    \
-    <br\> \
-    <h5> Preprints </h5>\
-    <h4>Highly Dynamic Quadruped Locomotion via Whole-Body Impulse Control and Model Predictive Control</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Jared Di Carlo, Benjamin Katz, Gerardo Bledt, and Sangbae Kim, in arXiv, 2019. <a href="papers/arxiv_wbic_mini-cheetah.pdf"> \[Paper]</a> </p> \
-    \
-    <h4>Dynamic Locomotion For Passive-Ankle Biped Robots And Humanoids Using Whole-Body Locomotion Control</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Jaemin Lee, Junhyeok Ahn, Jianwen Luo, and Luis Sentis, in arXiv, 2019. <a href="papers/arxiv_mercury.pdf"> \[Paper]</a> </p> \
-    \
-    <h4>Assessing Whole-Body Operational Space Control in a Point-Foot Series Elastic Biped: Balance on Split Terrain and Undirected Walking</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Luis Sentis, in arXiv, 2015. <a href="papers/arxiv_wbosc_hume.pdf"> \[Paper]</a> </p> \
-    \
-    <h4>Robust Dynamic Locomotion via Reinforcement Learning and Novel Whole Body Controller</h4> \
-    <p> <font color="olive">Donghyun Kim</font>, Jaemin Lee, Luis Sentis, in arXiv, 2017. <a href="papers/arxiv_reinforcement_learning.pdf"> \[Paper]</a> </p> \
-    </div> \
-    </div> \
-    </div> \
-    </div>\
+  <div id="publication" class="publication services">\
+  <!-- container -->\
+  <div class="container">\
+  <h3 class="w3layouts-title wow fadeInDown animated" data-wow-delay=".5s">Publication</h3> \
+  <div class="carousel-inner" role="listbox" style=" width:100%; height: 100% !important;"> \
+  <div class="publication-left"> </div> \
+  <div class="publication-right"> \
+  <h5>Journals</h5> \
+  <h4>Dynamic Locomotion For Passive-Ankle Biped Robots And Humanoids Using Whole-Body Locomotion Control</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Jaemin Lee, Junhyeok Ahn, Jianwen Luo, and Luis Sentis, International Journal of Robotics and Research, accpeted 2020. <a href="papers/ijrr_mercury.pdf"> \[Paper]</a> </p> \
+  \
+  <h4>Robust Bipedal Locomotion Based on a Hierarchical Control Structure</h4> \
+  <p> Jianwen Luo, Yao Su, Lecheng Ruan, Ye Zhao, <font color="olive"> Donghyun Kim</font>, Luis Sentis, and Chenglong Fu. Robotica, 2019. <a href="papers/robotica_biped.pdf"> \[Paper]</a> </p>\
+  \
+  <h4>Investigations of a Robotic Testbed with Viscoelastic Liquid Cooled Actuators </h4> \
+  <p> <font color="olive">Donghyun Kim</font>, J. Ahn, O. Campbell, N. Paine, L. Sentis, Transaction on Mechatronics, 2018. <a href="papers/TMech_2018.pdf"> \[Paper]</a> </p> \
+  \
+  <h4>Stabilizing Series-Elastic Point-Foot Bipeds Using Whole-Body Operational Space Control</h4> \
+  <p> [Finalist, Best WBC paper 2016] [Finalist, Best WBC-Video 2016] </p> \
+  <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Benito Fernandez, Luis Sentis, Transaction on Robotics, 2016. <a href="papers/tro.pdf"> \[Paper]</a> </p> \
+  \
+  <h4>Kinematic feedback control laws for generating natural arm movements</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Cheongjae Jang, Frank C. Park, Bioinspiration & Biomimetics, 2013. <a href="papers/kin_feedback_arm_movements.pdf"> \[Paper]</a> </p> \
+  \
+  \
+  <br\> \
+  <h5>Peer Reviewed Conference Proceedings</h5>\
+  <h4>Vision Aided Dynamic Exploration of Unstructured Terrain with a Small-Scale Quadruped Robot</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Daniel Carballo, Jared Di Carlo, Benjamine Katz, Gerardo Bledt, Bryan Lim, and Sangbae Kim, International Conference on Robotics and Automation (ICRA), 2020. <a href="papers/2020_ICRA_Vision.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Bi-Modal Hemispherical Sensor: A Unifying Solution for Three Axis Force and Contact Angle Measurement</h4> \
+  <p> Meng Yee Chuah, Lindsay Epstein, <font color="olive">Donghyun Kim</font>, Juan Romero, and Sangbae Kim, International Conference on Intelligent Robots and Systems (IROS), 2019. <a href="papers/2019_IROS_Sensor.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Control of A High Performance Bipedal Robot using Viscoelastic Liquid Cooled Actuators</h4> \
+  <p>Junhyeok Ahn, <font color="olive">Donghyun Kim</font>, Seunghyeon Bang, Nick Paine, and Luis Sentis, International Conference of Humanoid Robots, 2019. <a href="papers/2019_humanoid.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Control Scheme and Uncertainty Considerations for Dynamic Balancing of Passive-Ankled Bipeds and Full Humanoids</h4> \
+  <p> [Outstanding paper 2018]  </p> \
+  <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Hochul Hwang, and Luis Sentis, International Conference of Humanoid Robots, 2018. <a href="papers/2018_humanoid_mercury.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Computationally-Robust and Efficient Prioritized Whole-Body Controller with Contact Constraints </h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Jaemin Lee, Junhyeok Ahn, Orion Campbell, Hochul Hwang, and Luis Sentis, International Conference on Intelligent Robots and Systems (IROS), 2018. <a href="papers/2018_IROS_WBDC.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Fast Kinodynamic Bipedal Locomotion Planning with Moving Obstacles</h4> \
+  <p> Junhyeok Ahn, Orion Campbell, <font color="olive">Donghyun Kim</font>, and Luis Sentis, International Conference on Intelligent Robots and Systems (IROS), 2018. <a href="papers/2018_IROS_Planning.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Investigations of Viscoelastic Liquid Cooled Actuators Applied for Dynamic Motion Control of Legged Systems </h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Orion Campbell, Junhyeok Ahn, Luis Sentis, Nicholas Paine, International Conference of Humanoid Robots, 2017. <a href="papers/2017_humanoid_vlca.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Dynamic Behaviors on the NAO Robot With Closed-Loop Whole Body Operational Space Control</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Steven Jorgensen, Luis Sentis, International Conference of Humanoid Robots, 2016. <a href="papers/humanoid_nao.pdf"> [Paper]</a> <a href="poster/humanoid2016_poster.png"> \[Poster]</a> </p> \
+  \
+  <h4>A Method for Dynamically Balancing a Point Foot Robot</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Gray Thomas, Luis Sentis, International Conference of Humanoid Robots, 2015. <a href="papers/humanoid_2015_hume.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Continuous Cyclic Stepping on 3D Point-Foot Biped Robots Via Constant Time to Velocity Reversal</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Gray Thomas, Luis Sentis, 13th International Conference on Control, Automation, Robotics & Vision (ICARCV 2014) <a href="papers/ICARCV_2014_Hume.pdf"> [Paper]</a> </p> \
+  \
+  <h4>Empirical Modifications to a Phase Space Planner Which Compensates for Low Stiffness Actuation in a Planar, Point-foot, Biped Robot</h4> \
+  <p><font color="olive"> Donghyun Kim</font>, Ye Zhao, Gray Thomas, Proceedings of the ASME 2014 Dynamic Systems and Control Conference (DSCC 2014) <a href="papers/DSCC_2014_Hume.pdf"> [Paper]</a>  </p> \
+  \
+  <h4>Phase Space Planning and Robust Control for Data-Driven Locomotion Behaviors</h4> \
+  <p> Ye Zhao, <font color="olive">Donghyun Kim</font>, Benito Fernandez, Luis Sentis, International Conference of Humanoid Robots, 2013. <a href="papers/2013_humanoid.pdf"> [Paper]</a> </p> \
+  \
+  \
+  <br\> \
+  <h5>Abstracts and Posters</h5>\
+  <h4>Whole-Body Opertation Space Control: Undirectional Walking of a Point-Foot Series Elastic Biped</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Luis Sentis, Dynamic Walking, 2015. <a href="papers/DW15Abstract.pdf"> \[Paper]</a> \
+  \
+  <h4>Poster: Hybrid Multi-Contact Dynamics for Wedge Jumping Locomotion Behaviors</h4> \
+  <p> Ye Zhao, <font color="olive">Donghyun Kim</font>, Gray Thomas, Luis Sentis, HSCC, 2015. <a href="papers/HSCC-15-Locomotion-final.pdf"> \[Paper]</a> \
+  \
+  <h4>Towards Agility in Compliant Point-Foot Bipeds</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Luis Sentis, Dynamic Walking, 2014. <a href="papers/DW14Abstract.pdf"> \[Paper]</a> <a href="poster/Toward_Agility_in_Compliant_Point_Foot_Bipeds.pdf"> \[Poster]</a> </p> \
+  \
+  \
+  <br\> \
+  <h5> Preprints </h5>\
+  <h4>Highly Dynamic Quadruped Locomotion via Whole-Body Impulse Control and Model Predictive Control</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Jared Di Carlo, Benjamin Katz, Gerardo Bledt, and Sangbae Kim, in arXiv, 2019. <a href="papers/arxiv_wbic_mini-cheetah.pdf"> \[Paper]</a> </p> \
+  \
+  <h4>Assessing Whole-Body Operational Space Control in a Point-Foot Series Elastic Biped: Balance on Split Terrain and Undirected Walking</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Ye Zhao, Gray Thomas, Luis Sentis, in arXiv, 2015. <a href="papers/arxiv_wbosc_hume.pdf"> \[Paper]</a> </p> \
+  \
+  <h4>Robust Dynamic Locomotion via Reinforcement Learning and Novel Whole Body Controller</h4> \
+  <p> <font color="olive">Donghyun Kim</font>, Jaemin Lee, Luis Sentis, in arXiv, 2017. <a href="papers/arxiv_reinforcement_learning.pdf"> \[Paper]</a> </p> \
+  </div> \
+  </div> \
+  </div> \
+  </div>\
 ');
